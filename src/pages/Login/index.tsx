eadf1e16838d34de5684fe0871d6ff1b7c700b02
박신영 <psy_chopath@naver.com>
2024-09-22 01:07:38 +0900

    chore: 지도탐색시 돋보기 누르면 모달창 뜨게함

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,34 +1,19 @@
 import React, { useEffect } from 'react';
-import { Box, Button, VStack, Text } from '@chakra-ui/react';
+import { Button, VStack, Text } from '@chakra-ui/react';
 
 const LoginPage = () => {
-  // 카카오 SDK 초기화
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.Kakao) {
-      const { Kakao } = window as any;
-      if (!Kakao.isInitialized()) {
-        Kakao.init('YOUR_JAVASCRIPT_KEY'); // 여기에 카카오 JavaScript 키를 입력할것
-      }
-    } else {
-      console.error("Kakao SDK가 로드되지 않았습니다.");
-    }
-  }, []);
-
-  // 카카오 로그인 처리 함수
+  
+  // 카카오 로그인 요청 함수
   const handleKakaoLogin = () => {
-    if (typeof window !== 'undefined' && window.Kakao) {
-      const { Kakao } = window as any;
-      Kakao.Auth.authorize({
-        redirectUri: 'http://{기영이가 열고있는 프론트엔드 서버 주소}/token.html', // 로그인 성공 시 리다이렉트될 URL
-      });
-    }
+    // 로그인 요청 시 페이지 리다이렉트
+    window.location.href = 'http://43.203.172.143:8080/api/auth/oauth/kakao';
   };
 
   return (
     <VStack spacing={4} align="center" justify="center" h="100vh">
-      <Box p={4} shadow="md" borderWidth="1px">
-        <Text>서비스를 이용하시려면 로그인해주세요</Text>
-      </Box>
+      <Text>서비스를 이용하시려면 로그인해주세요</Text>
+      <Text>밑에 버튼 누르면 지호가 카톡방에 올린 링크로 넘어갈뿐임</Text>
+
       <Button
         colorScheme="yellow"
         onClick={handleKakaoLogin}
