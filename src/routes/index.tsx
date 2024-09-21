b08569110dd9e2e0d9017a633ac65cb6fe7a7151
박신영 <psy_chopath@naver.com>
2024-09-21 19:16:12 +0900

    chore: 로그인 화면은 네브바 필요없음

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,23 @@ import { RouterPath } from './path';
 import { Layout } from '@/components/Layout';
 import { MainPage } from '@/pages/Main';
 import MyPage from '@/pages/Mypage';
-import Login from '@/pages/Login'
+import Login from '@/pages/Login';
 import Directions from '@/pages/Directions';
 
 const router = createBrowserRouter([
+  // Login route without Layout
+  {
+    path: RouterPath.login,
+    element: <Login />,
+  },
+  // Routes with Layout
   {
     path: RouterPath.root,
     element: <Layout />,
     children: [
-      { path: RouterPath.main, element: <MainPage /> }, 
-      { path: RouterPath.mypage, element: <MyPage />},
-      { path: RouterPath.login, element: <Login />},
-      { path: RouterPath.directions, element: <Directions />}
+      { path: RouterPath.main, element: <MainPage /> },
+      { path: RouterPath.mypage, element: <MyPage /> },
+      { path: RouterPath.directions, element: <Directions /> },
     ],
   },
 ]);
