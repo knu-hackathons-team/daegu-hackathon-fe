0f53b80fa9cb51987cb45772f8872a003c6bbb28
박신영 <psy_chopath@naver.com>
2024-09-21 22:29:18 +0900

    feat: navbar 디자인 변경 타원형애니메이션추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,28 @@
+/** @jsxImportSource @emotion/react */
 import { Contents } from "@/components/MainPage/Contents";
+import { Flex } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 
 export const MainPage = () => {
   return (
     <Wrapper>
-      <Contents />
+      <InnerWrapper>
+        <Contents />
+      </InnerWrapper>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled(Flex)`
   width: 100%;
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+`;
+
+const InnerWrapper = styled.div`
+  width: 100%;
+  max-width: 600px;  // 내부 콘텐츠의 최대 너비를 설정하여 일관성 유지
+  padding: 20px;
+  box-sizing: border-box;
 `;
